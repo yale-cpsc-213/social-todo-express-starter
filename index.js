cd7d24510ef9168dc5e45c325482adc4dd8c890d
Kyle L. Jensen <kljensen@gmail.com>
2018-02-03 07:47:46 -0500

    Eslint

--- index.js
+++ index.js
@@ -16,76 +16,92 @@ const Tasks = require('./models/tasks.js');
 
 // Configure our app
 const store = new MongoDBStore({
-  uri: process.env.MONGO_URL,
-  collection: 'sessions',
+    uri: process.env.MONGO_URL,
+    collection: 'sessions',
 });
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
+    defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
+
+// For parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
-  extended: true,
-})); // for parsing application/x-www-form-urlencoded
+    extended: true,
+}));
+
 // Configure session middleware that will parse the cookies
 // of an incoming request to see if there is a session for this cookie.
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: 'auto',
-  },
-  store,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto',
+    },
+    store,
 }));
 
 // Middleware that looks up the current user for this sesssion, if there
 // is one.
 app.use((req, res, next) => {
-  if (req.session.userId) {
-    Users.findById(req.session.userId, (err, user) => {
-      if (!err) {
-        res.locals.currentUser = user;
-      }
-      next();
-    });
-  } else {
-    next();
-  }
+    if (req.session.userId) {
+        Users.findById(req.session.userId, (err, user) => {
+            if (!err) {
+                res.locals.currentUser = user;
+            }
+            next();
+        });
+    } else {
+        next();
+    }
 });
 
-// Middleware that checks if a user is logged in. If so, the
-// request continues to be processed, otherwise a 403 is returned.
+/**
+ * Middleware that checks if a user is logged in.
+ * If so, the request continues to be processed, otherwise a
+ * 403 is returned.
+ * @param  {Request} req - The request
+ * @param  {Response} res - sdf
+ * @param  {Function} next - sdfs
+ * @returns {undefined}
+ */
 function isLoggedIn(req, res, next) {
-  if (res.locals.currentUser) {
-    next();
-  } else {
-    res.sendStatus(403);
-  }
+    if (res.locals.currentUser) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
 }
 
-// Middleware that loads a users tasks if they are logged in.
+/**
+ * Middleware that loads a users tasks if they are logged in.
+ * @param  {Request} req - An express request object
+ * @param  {Response} res - An express response object
+ * @param  {Callback} next - Called when the function is done
+ * @returns {undefined}
+ */
 function loadUserTasks(req, res, next) {
-  // Removed
-  next();
+    // Removed
+    next();
 }
 
 // Return the home page after loading tasks for users, or not.
 app.get('/', loadUserTasks, (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 // Handle submitted form for new users
 app.post('/user/register', (req, res) => {
-  res.send('woot');
+    res.send('woot');
 });
 
 app.post('/user/login', (req, res) => {
-  res.send('woot');
+    res.send('woot');
 });
 
 // Log a user out
 app.get('/user/logout', (req, res) => {
-  res.send('woot');
+    res.send('woot');
 });
 
 //  All the controllers and routes below this require
@@ -94,19 +110,20 @@ app.use(isLoggedIn);
 
 // Handle submission of new task form
 app.post('/tasks/:id/:action(complete|incomplete)', (req, res) => {
-  res.send('woot');
+    res.send('woot');
 });
 
 app.post('/tasks/:id/delete', (req, res) => {
-  res.send('woot');
+    res.send('woot');
 });
 
 // Handle submission of new task form
 app.post('/task/create', (req, res) => {
-  res.send('woot');
+    res.send('woot');
 });
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+    // eslint-disable-next-line no-console
+    console.log(`Example app listening on port ${process.env.PORT}`);
 });
--- models/tasks.js
+++ models/tasks.js
@@ -2,21 +2,23 @@
 
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const {
+    Schema,
+    ObjectId,
+} = mongoose;
 
 const stringField = {
-  type: String,
-  minlength: 1,
-  maxlength: 500,
+    type: String,
+    minlength: 1,
+    maxlength: 500,
 };
 
 const TaskSchema = new Schema({
-  owner: ObjectId,
-  name: stringField,
-  description: stringField,
-  isComplete: Boolean,
-  collaborators: [String],
+    owner: ObjectId,
+    name: stringField,
+    description: stringField,
+    isComplete: Boolean,
+    collaborators: [String],
 });
 
 module.exports = mongoose.model('Tasks', TaskSchema);
